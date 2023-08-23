7f014c9e745a28ad289ba8c2afc0c9433851b1e6
ayakaman <ayakaman@example.com>
2023-08-23 06:31:03 +0000

    登録処理まで書いた

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ShoppingListRegisterPostRequest;
+use App\Models\Shopping_list as Shopping_listModel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -16,5 +18,30 @@ class ShoppingListController extends Controller
     public function list()
     {
         return view('shopping_list.list');
+    }
+
+    /**
+     * タスクの新規登録
+     */
+    public function register(ShoppingListRegisterPostRequest $request)
+    {
+        // validate済みのデータの取得
+        $datum = $request->validated();
+        //var_dump($datum); exit;
+        $datum['user_id'] = Auth::id();
+
+        // INSERT
+        try {
+            $r = Shopping_listModel::create($datum);
+        } catch(\Throwable $e) {
+            echo $e->getMessage();
+            exit;
+        }
+
+        // タスク登録成功
+        $request->session()->flash('front.shoppinglist_register_success', true);
+
+        //
+        return redirect('/shopping_list/list');
     }
 }
