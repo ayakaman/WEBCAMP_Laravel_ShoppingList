943e161b3c412ce77746f6fc00dd92a52de971c2
ayakaman <ayakaman@example.com>
2023-09-01 14:23:58 +0000

    ユーザ登録画面を修正

--- app/Http/Requests/UserRegisterPost.php
+++ app/Http/Requests/UserRegisterPost.php
@@ -15,7 +15,7 @@ class UserRegisterPost extends FormRequest
     {
         return [
             'name' => ['required', 'max:128'],
-            'email' => ['required', 'email', 'max:254'],
+            'email' => ['required', 'email', 'max:254', 'unique:users'],
             'password' => ['required', 'confirmed', 'max:72'],
             'password_confirmation' => ['required', 'max:72'],
         ];
