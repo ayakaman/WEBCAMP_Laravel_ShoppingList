fa52d6db9458175e72f1351afbfc64e44ad28ebe
ayakaman <ayakaman@example.com>
2023-09-01 11:07:07 +0000

    管理タスク書いた

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -2,11 +2,5 @@
 
 {{-- メインコンテンツ --}}
 @section('contets')
-        <menu label="リンク">
-        管理画面Top<br>
-        <a href="/admin/user/list">ユーザ一覧</a><br>
-        <a href="/admin/logout">ログアウト</a><br>
-        </menu>
-
         <h1>管理画面</h1>
 @endsection
